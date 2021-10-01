fe812d53a82d1f5166221cf5e625ab45b800245a
KFrost <kristan.frost@gmail.com>
2021-10-01 11:25:08 -0500

    creating commands with yargs

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 
-// const getNotes = require("./notes.js")
-// const validator = require("validator");
 const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes.js")
 
-// console.log(getNotes());
-// console.log(validator.isEmail("test@test.com"));
-console.log(chalk.bgGreen.bold("Success!"));
+yargs.version("1.1.0");
+
+// Create add command
+yargs.command({
+    command: "add", 
+    describe: "Add a new note",
+    handler: function(){
+        console.log("Adding a new note");
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: function(){
+        console.log("Removing the note.");
+    }
+})
+
+// Create list command
+yargs.command({
+    command: "list",
+    describe: "List all notes.",
+    handler: function(){
+        console.log("Listing all notes.");
+    }
+})
+
+// Create read command
+yargs.command({
+    command:"read",
+    describe: "Read a note.",
+    handler: function(){
+        console.log("Here is your note.");
+    }
+})
+
+console.log(yargs.argv);
+
+
 
